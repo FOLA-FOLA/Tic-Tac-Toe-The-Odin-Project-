8c28233b861b843404b933142d7cf05df89dbc80
fola-fola <ogunseijufolakemi@gmail.com>
2025-08-11 15:14:30 +0100

    Add entry form

---
+++ script.js
@@ -0,0 +1,33 @@
+const players = []
+const gamemarkers = []
+
+const formsubmitbtn = document.getElementById('formsubmitbtn')
+const entryform = document.getElementById('gameform')
+
+function addPlayersToLibrary() {
+    const formdeets = new FormData(entryform);
+    const players = {
+        playerx: formdeets.get('playerxname'),
+        playero: formdeets.get('playeroname')
+    };
+    players.push(players);
+    return players;
+}
+function showgame() {
+    const gamebody = document.getElementsById('gamebody')
+    const instruction = document.getElementById('instruction')
+    gamebody.classList.remove('hidden')
+    instruction.classList.remove('hidden')
+}
+function hidegame() {
+    const gamebody = document.getElementsById('gamebody')
+    const instruction = document.getElementById('instruction')
+    gamebody.classList.add('hidden')
+    instruction.classList.add('hidden')
+}
+
+formsubmitbtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    addPlayersToLibrary()
+    showgame();
+})
