aef17c0c03dfcbdd489972304e95103ac9d3f94b
fola-fola <ogunseijufolakemi@gmail.com>
2025-08-11 16:56:03 +0100

    Fix and add gameboard

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ const gamemarkers = []
 
 const formsubmitbtn = document.getElementById('formsubmitbtn')
 const entryform = document.getElementById('gameform')
+const tttboard = document.getElementById('tttboard')
 
 function addPlayersToLibrary() {
     const formdeets = new FormData(entryform);
-    const players = {
+    const playerData = {
         playerx: formdeets.get('playerxname'),
         playero: formdeets.get('playeroname')
     };
-    players.push(players);
-    return players;
+    players.push(playerData);
+    return playerData;
 }
 function showgame() {
-    const gamebody = document.getElementsById('gamebody')
+    const gamebody = document.getElementById('gamebody')
     const instruction = document.getElementById('instruction')
     gamebody.classList.remove('hidden')
     instruction.classList.remove('hidden')
 }
+
 function hidegame() {
     const gamebody = document.getElementById('gamebody')
     const instruction = document.getElementById('instruction')
@@ -29,5 +31,20 @@ function hidegame() {
 formsubmitbtn.addEventListener('click', (e) => {
     e.preventDefault()
     addPlayersToLibrary()
+    entryform.classList.add('hidden')
     showgame();
 })
+function addmove() {
+    const formdeets = new FormData(entryform);
+    const playerData = {
+        playerx: formdeets.get('playerxname'),
+        playero: formdeets.get('playeroname')
+    };
+    players.push(playerData);
+    return playerData;
+}
+tttboard.addEventListener('click', (event) => {
+    if (event.target.classList === 'slot') {
+        
+    }
+});
